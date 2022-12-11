195b9c449ab7b3ceaba0f0120d0cb88b5720ce7f
ttt <ttt@ttts-MBP.fritz.box>
2022-12-11 16:04:15 +0100

    day 10 part1 and part 2 done yay

--- main.cpp
+++ main.cpp
@@ -16,7 +16,60 @@
 #include <range/v3/all.hpp>
 #include "../aoc/util/util.hpp"
 #include <array>
+std::tuple<int, int> range_in_values(int current_row){
+    int end = 0;
+    int start = 0;
+    if(current_row == 0){
+        start = 0;
+        end = 40;
+    }
+    else if(current_row == 1){
+        start = 40;
+        end = 80;
+    }
+    else if(current_row == 2){
+        start = 80;
+        end = 120;
+    }
+    else if(current_row == 3){
+        start = 120;
+        end = 160;
+    }
+    else if(current_row == 4){
+        start = 160;
+        end = 200;
+    }
+    else if(current_row == 5){
+        start = 200;
+        end = 240;
+    }
+    return std::make_tuple(start, end);
 
+}
+void row(std::vector<int> values, int current_row){
+    const auto [start, end] = range_in_values(current_row);
+
+    for(int i = start; i < end; i++){
+        int sprite =values[i];
+        if(i%40 == (sprite-1) || i%40 == sprite || i%40 == (sprite+1) ){
+            std::cout<<"#";
+        }
+        else{
+            std::cout<<".";
+        }
+    }
+    std::cout<<"\n";
+}
+
+void drawer(std::vector<int> values ){
+
+  int screen_size = 6;
+  for(int j = 0 ; j < screen_size; ++j ){
+    row(values , j);
+  }
+}
+
+// Part 1 Output
 int output(int cycle, int count){
 
             if(cycle == 20){
@@ -51,7 +104,9 @@ std::tuple<std::string, std::string> parseInstr(std::string str){
 
     auto tokens =  SplitString(str, ' ');
     return std::make_tuple(tokens[0] , tokens[1]);
+
 }
+
 int main()
 	{
         int sum = 0;
@@ -65,11 +120,14 @@ int main()
             }
 
             const auto [instruction, value] = parseInstr(line);
+            X.push_back(X.back());
+
             if(instruction=="noop") {cycle++;
                sum += output(cycle, count);
             }
             else if(instruction=="addx"){
                 int x = std::stoi(value);
+                X.push_back(X.back()+x);
                 if(x < 0){
                     int range = x*-1;
                     for(int i = 0; i <2; ++i){
@@ -88,5 +146,9 @@ int main()
                 }
             }
         }
+        // Part1
         std::cout << sum;
+        for(int k : X ) std::cout<< k << "\n";
+        // Part2
+        drawer(X);
     }
